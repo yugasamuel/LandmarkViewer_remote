947707eb4f0682486123dea66bf9b564db71b40d
Yuga Samuel <yugasamuel2002@gmail.com>
2023-03-22 13:08:23 +0700

    Initial Commit

---
+++ LandmarkViewer/LandmarkViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkViewerApp.swift
+//  LandmarkViewer
+//
+//  Created by Yuga Samuel on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
