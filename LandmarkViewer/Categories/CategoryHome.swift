51f76027688be0ae215bd236cbcb3904838a4e12
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-12 00:00:48 +0700

    Update: Category home

--- LandmarkViewer/Categories/CategoryHome.swift
+++ LandmarkViewer/Categories/CategoryHome.swift
@@ -18,11 +18,14 @@ struct CategoryHome: View {
                                    .scaledToFill()
                                    .frame(height: 200)
                                    .clipped()
+                                   .listRowInsets(EdgeInsets())
 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
+            .listStyle(.plain)
             .navigationTitle("Featured")
         }
     }
